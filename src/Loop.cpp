d733d790d3e4f166d103dd576abbb848375ca833
Gustavo Garcia <gustavogb@gmail.com>
2017-01-23 01:31:54 +0100

    FIXES

--- src/Loop.cpp
+++ src/Loop.cpp
@@ -9,8 +9,6 @@
 using uvcpp::Loop;
 
 Loop::Loop(): loop_(new uv_loop_t, [](uv_loop_t *loop) { uv_loop_close(loop); delete loop; }) {
-  // Testing valgrind works
-  // new uv_loop_t();
   uv_loop_init(loop_.get());
 }
 
